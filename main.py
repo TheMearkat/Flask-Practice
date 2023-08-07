daef9cade7f33b0e911112ebc814d4f390670774
Mitchell Mears <Mitchell.Mears@yahoo.com>
2023-08-07 16:18:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from hello_world import app
+from app import app  
+import random
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=random.randint(2000, 9000)
+    )
